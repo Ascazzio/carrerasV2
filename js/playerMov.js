449b11f57587c072548f59d261acf25aa82ea511
Fabrizio Pironi <fabriziopironi@gmail.com>
2021-10-21 14:00:01 +0200

    Cambio de concepto, drag strip

--- js/playerMov.js
+++ js/playerMov.js
@@ -7,67 +7,69 @@ const keyEvent = (event) => {
     switch(event.keyCode){
         case 32:
             console.log("pulsaste la barra espaciadora");
-            document.getElementById("trafficCenter").classList.add("trafficEnd");
-document.getElementById("trafficRight").classList.add("trafficEnd");
-document.getElementById("trafficLeft").classList.add("trafficEnd");
-            
+            document.getElementById("player").src = `img/${playerCars[0]}R.png`;
+
         break;
 
-        //Derecha / right
-        case 39:
-            console.log("pulsaste la flecha derecha");
+        case 115: //Letra "s"
+            console.log("pulsaste la (s)" );
+        break;
+
+        // //Derecha / right
+        // case 39:
+        //     console.log("pulsaste la flecha derecha");
             
-            if (document.getElementById("player").classList.contains("left")) {  
-                                                                                        // Si se encuentra en la derecha
-                document.getElementById("player").classList.remove("left");             // Pase al medio
-                document.getElementById("player").src = `img/${playerCars[0]}C.png`;
+        //     if (document.getElementById("player").classList.contains("left")) {  
+        //                                                                                 // Si se encuentra en la derecha
+        //         document.getElementById("player").classList.remove("left");             // Pase al medio
+        //         document.getElementById("player").src = `img/${playerCars[0]}C.png`;
 
-                setTimeout(() => {
-                    carril = 2;
-                    console.log("carril =" + carril);    
+        //         setTimeout(() => {
+        //             carril = 2;
+        //             console.log("carril =" + carril);    
     
-                }, 250);
+        //         }, 250);
 
-            } else {
-                document.getElementById("player").src = `img/${playerCars[0]}R.png`;
-                document.getElementById("player").classList.add("right");                // Si ya se encuentra en el centro
+        //     } else {
+        //         document.getElementById("player").src = `img/${playerCars[0]}R.png`;
+        //         document.getElementById("player").classList.add("right");                // Si ya se encuentra en el centro
                                                                                         
-                setTimeout(() => {                                                      // Entoces cambiar a la derecha
-                    carril = 3;
-                    console.log("carril =" + carril);    
+        //         setTimeout(() => {                                                      // Entoces cambiar a la derecha
+        //             carril = 3;
+        //             console.log("carril =" + carril);    
     
-                }, 250);
+        //         }, 250);
 
-            }
+        //     }
             
-        break;
+        // break;
 
-        //Izquierda / left
-        case 37:
-            console.log("pulsaste la flecha izquierda");
-            // document.getElementById("player").src = `img/${playerCars[0]}L.png`;
+        // //Izquierda / left
+        // case 37:
+        //     console.log("pulsaste la flecha izquierda");
+        //     // document.getElementById("player").src = `img/${playerCars[0]}L.png`;
 
-            if (document.getElementById("player").classList.contains("right")) {
+        //     if (document.getElementById("player").classList.contains("right")) {
 
-                document.getElementById("player").classList.remove("right");
-                document.getElementById("player").src = `img/${playerCars[0]}C.png`;
+        //         document.getElementById("player").classList.remove("right");
+        //         document.getElementById("player").src = `img/${playerCars[0]}C.png`;
 
-                setTimeout(() => {
-                    carril = 2;
-                    console.log("carril =" + carril);    
-                }, 250);
+        //         setTimeout(() => {
+        //             carril = 2;
+        //             console.log("carril =" + carril);    
+        //         }, 250);
 
-            } else {
-                document.getElementById("player").src = `img/${playerCars[0]}L.png`;
-                document.getElementById("player").classList.add("left");
+        //     } else {
+        //         document.getElementById("player").src = `img/${playerCars[0]}L.png`;
+        //         document.getElementById("player").classList.add("left");
 
-                setTimeout(() => {
-                    carril = 1;
-                    console.log("carril =" + carril);    
-                }, 250);
-            }
+        //         setTimeout(() => {
+        //             carril = 1;
+        //             console.log("carril =" + carril);    
+        //         }, 250);
+        //     }
 
-        break;
+        // break;
 
         default:
             console.log("Movimineto invalido");
